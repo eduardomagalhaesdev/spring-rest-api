853e28baff3d5d1ff20133a850ce97425fb5fb59
Eduardo Magalhaes <52082458+edudevhome@users.noreply.github.com>
2022-03-24 20:11:54 -0300

    Update Cliente.java

--- fastlog-api/src/main/java/br/com/spring/fastlog/domain/model/Cliente.java
+++ fastlog-api/src/main/java/br/com/spring/fastlog/domain/model/Cliente.java
@@ -4,11 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Include;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Entity
 @Getter
